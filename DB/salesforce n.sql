fe3c362fcf314cb686cdeeffdf479beffb8f22f5
ijidev <iheanyichukwui94@gmail.com>
2023-09-19 09:39:27 +0100

    last update

--- DB/salesforce n.sql
+++ DB/salesforce n.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 19, 2023 at 10:38 AM
+-- Generation Time: Sep 16, 2023 at 07:45 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -36,6 +36,13 @@ CREATE TABLE `deposits` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `deposits`
+--
+
+INSERT INTO `deposits` (`id`, `proof`, `is_approved`, `amount`, `user_id`, `created_at`, `updated_at`) VALUES
+(1, 'paymentproof5.png', 1, 99, 5, '2023-09-08 02:40:14', '2023-09-11 03:18:01');
 
 -- --------------------------------------------------------
 
@@ -72,7 +79,8 @@ CREATE TABLE `faqs` (
 --
 
 INSERT INTO `faqs` (`id`, `question`, `answer`, `created_at`, `updated_at`) VALUES
-(3, 'How Many Accounts Can I Open?', 'A User Can Only Have One Account', '2023-09-19 07:26:04', '2023-09-19 07:26:04');
+(1, 'what is salesforce', 'this is sales force in the force of forces', NULL, NULL),
+(2, 'what i want', 'what you want is what you get', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -96,17 +104,18 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 (5, '2023_08_30_092539_create_tiers_table', 1),
-(6, '2023_08_30_092540_create_users_table', 1),
-(7, '2023_08_31_060817_laratrust_setup_tables', 1),
-(8, '2023_09_05_064059_create_and_update_table', 1),
-(9, '2023_09_08_024422_create_deposits_table', 1),
-(10, '2023_09_08_094456_create_payment_infos_table', 1),
-(11, '2023_09_08_115243_create_user_payments_table', 1),
-(12, '2023_09_09_190802_create_withdrawals_table', 1),
-(13, '2023_09_09_205044_create_settings_table', 1),
-(14, '2023_09_11_221741_table_update', 1),
-(15, '2023_09_13_153723_create_faqs_table', 1),
-(16, '2023_09_16_050722_create_notifications_table', 1);
+(6, '2023_08_31_060817_laratrust_setup_tables', 1),
+(7, '2023_09_05_064059_create_and_update_table', 1),
+(8, '2024_10_12_000000_create_users_table', 1),
+(9, '2023_09_08_024422_create_deposits_table', 2),
+(10, '2023_09_08_094456_create_payment_infos_table', 3),
+(11, '2023_09_08_115243_create_user_payments_table', 4),
+(12, '2023_09_09_190802_create_withdrawals_table', 5),
+(15, '2023_09_09_205044_create_settings_table', 6),
+(16, '2023_8_31_000000_create_users_table', 6),
+(17, '2023_09_11_221741_table_update', 7),
+(18, '2023_09_13_153723_create_faqs_table', 8),
+(20, '2023_09_16_050722_create_notifications_table', 9);
 
 -- --------------------------------------------------------
 
@@ -123,6 +132,13 @@ CREATE TABLE `notifications` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `title`, `massage`, `user_id`, `is_read`, `created_at`, `updated_at`) VALUES
+(1, 'Withdrawal Request', 'Withdrawal Request submitted successfuly', 5, 1, '2023-09-16 16:11:35', '2023-09-16 16:43:47');
 
 -- --------------------------------------------------------
 
@@ -157,11 +173,20 @@ CREATE TABLE `password_reset_tokens` (
 CREATE TABLE `payment_infos` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `wallet_name` varchar(255) NOT NULL,
-  `walet_address` varchar(255) NOT NULL,
+  `wallet_address` varchar(255) NOT NULL,
   `status` enum('active','inactive') NOT NULL DEFAULT 'active',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `payment_infos`
+--
+
+INSERT INTO `payment_infos` (`id`, `wallet_name`, `wallet_address`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'BITCOIN', 'uygduygbfdu762i743gtydg76fdwyefguywegv', 'active', NULL, NULL),
+(2, 'TRC-20', 'hfhbsffwgfuygwafugwuyfgewarygfuywag', 'active', NULL, NULL),
+(3, 'ERC-20', ',kqHFVDWCHVWQHFDCWIQFVCWDC J', 'inactive', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -183,16 +208,16 @@ CREATE TABLE `permissions` (
 --
 
 INSERT INTO `permissions` (`id`, `name`, `display_name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'users-create', 'Create Users', 'Create Users', '2023-09-18 14:13:39', '2023-09-18 14:13:39'),
-(2, 'users-read', 'Read Users', 'Read Users', '2023-09-18 14:13:39', '2023-09-18 14:13:39'),
-(3, 'users-update', 'Update Users', 'Update Users', '2023-09-18 14:13:39', '2023-09-18 14:13:39'),
-(4, 'users-delete', 'Delete Users', 'Delete Users', '2023-09-18 14:13:39', '2023-09-18 14:13:39'),
-(5, 'payments-create', 'Create Payments', 'Create Payments', '2023-09-18 14:13:39', '2023-09-18 14:13:39'),
-(6, 'payments-read', 'Read Payments', 'Read Payments', '2023-09-18 14:13:39', '2023-09-18 14:13:39'),
-(7, 'payments-update', 'Update Payments', 'Update Payments', '2023-09-18 14:13:39', '2023-09-18 14:13:39'),
-(8, 'payments-delete', 'Delete Payments', 'Delete Payments', '2023-09-18 14:13:40', '2023-09-18 14:13:40'),
-(9, 'profile-read', 'Read Profile', 'Read Profile', '2023-09-18 14:13:40', '2023-09-18 14:13:40'),
-(10, 'profile-update', 'Update Profile', 'Update Profile', '2023-09-18 14:13:40', '2023-09-18 14:13:40');
+(1, 'users-create', 'Create Users', 'Create Users', '2023-09-05 20:46:34', '2023-09-05 20:46:34'),
+(2, 'users-read', 'Read Users', 'Read Users', '2023-09-05 20:46:34', '2023-09-05 20:46:34'),
+(3, 'users-update', 'Update Users', 'Update Users', '2023-09-05 20:46:35', '2023-09-05 20:46:35'),
+(4, 'users-delete', 'Delete Users', 'Delete Users', '2023-09-05 20:46:35', '2023-09-05 20:46:35'),
+(5, 'payments-create', 'Create Payments', 'Create Payments', '2023-09-05 20:46:36', '2023-09-05 20:46:36'),
+(6, 'payments-read', 'Read Payments', 'Read Payments', '2023-09-05 20:46:36', '2023-09-05 20:46:36'),
+(7, 'payments-update', 'Update Payments', 'Update Payments', '2023-09-05 20:46:36', '2023-09-05 20:46:36'),
+(8, 'payments-delete', 'Delete Payments', 'Delete Payments', '2023-09-05 20:46:36', '2023-09-05 20:46:36'),
+(9, 'profile-read', 'Read Profile', 'Read Profile', '2023-09-05 20:46:36', '2023-09-05 20:46:36'),
+(10, 'profile-update', 'Update Profile', 'Update Profile', '2023-09-05 20:46:36', '2023-09-05 20:46:36');
 
 -- --------------------------------------------------------
 
@@ -274,8 +299,8 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `display_name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'Admin', 'Admin', '2023-09-18 14:13:39', '2023-09-18 14:13:39'),
-(2, 'user', 'User', 'User', '2023-09-18 14:13:42', '2023-09-18 14:13:42');
+(1, 'admin', 'Admin', 'Admin', '2023-09-05 20:46:34', '2023-09-05 20:46:34'),
+(2, 'user', 'User', 'User', '2023-09-05 20:46:39', '2023-09-05 20:46:39');
 
 -- --------------------------------------------------------
 
@@ -294,8 +319,9 @@ CREATE TABLE `role_user` (
 --
 
 INSERT INTO `role_user` (`role_id`, `user_id`, `user_type`) VALUES
-(1, 2, 'App\\Models\\User'),
-(2, 3, 'App\\Models\\User');
+(1, 1, 'App\\Models\\User'),
+(2, 5, 'App\\Models\\User'),
+(2, 6, 'App\\Models\\User');
 
 -- --------------------------------------------------------
 
@@ -315,6 +341,13 @@ CREATE TABLE `settings` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `ref_amount`, `min_withdrawal`, `active_hour`, `close_hour`, `about`, `term`, `created_at`, `updated_at`) VALUES
+(1, '20', 0.00, '12:00:00', '22:00:00', NULL, 'terms \r\n- and con \r\n-jj', '2023-09-11 04:38:15', '2023-09-13 14:31:25');
+
 -- --------------------------------------------------------
 
 --
@@ -324,6 +357,7 @@ CREATE TABLE `settings` (
 CREATE TABLE `tiers` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
   `icon` varchar(255) DEFAULT NULL,
   `price` int(11) NOT NULL,
   `percent` double(8,2) NOT NULL DEFAULT 0.00,
@@ -337,12 +371,10 @@ CREATE TABLE `tiers` (
 -- Dumping data for table `tiers`
 --
 
-INSERT INTO `tiers` (`id`, `name`, `icon`, `price`, `percent`, `reset`, `daily_optimize`, `created_at`, `updated_at`) VALUES
-(1, 'Normal', 'uploads/icon/Normal.png', 99, 0.50, 3, 35, '2023-09-19 04:49:11', '2023-09-19 04:49:11'),
-(2, 'Silver', 'uploads/icon/Silver.png', 399, 0.75, 3, 40, '2023-09-19 04:51:35', '2023-09-19 04:51:35'),
-(3, 'Gold', 'uploads/icon/Gold.png', 999, 1.00, 3, 45, '2023-09-19 04:52:27', '2023-09-19 04:52:27'),
-(4, 'Platinum', 'uploads/icon/Platinum.png', 4999, 1.25, 3, 50, '2023-09-19 04:54:28', '2023-09-19 04:54:28'),
-(5, 'Diamond', 'uploads/icon/Diamond.png', 9999, 1.50, 3, 55, '2023-09-19 04:56:39', '2023-09-19 04:56:39');
+INSERT INTO `tiers` (`id`, `name`, `description`, `icon`, `price`, `percent`, `reset`, `daily_optimize`, `created_at`, `updated_at`) VALUES
+(1, 'starter', 'some random info on this tier \n- point 1 of points\n- point 2 of points\n- point 3 of points\n- point 4 of points', 'uploads/icon/John duo.png', 99, 0.50, 3, 30, NULL, NULL),
+(2, 'level 2', 'some random info on this tier \n- point 1 of points 1\n- point 2 of points 2\n- point 3 of points 3\n- point 4 of points 4', 'uploads/icon/John duo.png', 300, 1.50, 3, 30, NULL, NULL),
+(3, 'John duo', 'hvibikbbv hj vhvgh vhjv hj vhygv', 'uploads/icon/John duo.png', 654, 0.00, 3, 30, '2023-09-11 12:30:15', '2023-09-11 12:30:15');
 
 -- --------------------------------------------------------
 
@@ -352,9 +384,8 @@ INSERT INTO `tiers` (`id`, `name`, `icon`, `price`, `percent`, `reset`, `daily_o
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `ref_id` varchar(250) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
-  `user_id` bigint(20) UNSIGNED NOT NULL,
-  `ref_id` varchar(255) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `pass` varchar(255) NOT NULL,
@@ -365,22 +396,23 @@ CREATE TABLE `users` (
   `frozen` double(8,2) NOT NULL DEFAULT 0.00,
   `asset` double(8,2) NOT NULL DEFAULT 0.00,
   `reset_count` int(11) NOT NULL DEFAULT 0,
-  `credit_score` int(11) NOT NULL DEFAULT 0,
+  `credit_score` int(11) NOT NULL DEFAULT 100,
   `is_active` tinyint(1) NOT NULL DEFAULT 0,
   `tier_id` bigint(20) UNSIGNED DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `ref` bigint(20) UNSIGNED DEFAULT NULL,
+  `user_id` bigint(20) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `user_id`, `ref_id`, `email`, `email_verified_at`, `pass`, `password`, `remember_token`, `optimized`, `balance`, `frozen`, `asset`, `reset_count`, `credit_score`, `is_active`, `tier_id`, `created_at`, `updated_at`) VALUES
-(1, 'user', 1, 'ref_01', '1111', NULL, '1234567', '1234567', NULL, 0, 0.00, 0.00, 0.00, 0, 100, 1, NULL, NULL, NULL),
-(2, 'Admin', 1, 'ref_02', 'Admin@salesforcereviews.info', NULL, '12345678', '$2y$10$5x07oAoTfdnPnZKEGW5ECehbMnZ/VpteDl1iEnAqs05YUPlnry7wK', NULL, 0, 0.00, 0.00, 0.00, 0, 0, 0, NULL, '2023-09-18 14:28:10', '2023-09-18 14:28:10'),
-(3, 'User', 2, 'ref_03', 'user@user.com', NULL, '12345678', '$2y$10$NefGFgifBGWInkqxr2y1EuIvvSFRlhaqPOM53/CieKAsaAIMb3leG', NULL, 0, 0.00, 0.00, 0.00, 0, 100, 1, 2, '2023-09-18 14:53:06', '2023-09-19 05:41:29'),
-(4, 'User', 2, NULL, 'user2@user.com', NULL, '12345678', '$2y$10$OHrp/abObtSvYeGmE6z.aOvRy.hY75PSXbJHZg4Om6FxRc2pgw2lS', NULL, 0, 0.00, 0.00, 0.00, 0, 0, 0, NULL, '2023-09-18 15:18:12', '2023-09-18 15:18:12');
+INSERT INTO `users` (`id`, `ref_id`, `name`, `email`, `email_verified_at`, `pass`, `password`, `remember_token`, `optimized`, `balance`, `frozen`, `asset`, `reset_count`, `credit_score`, `is_active`, `tier_id`, `created_at`, `updated_at`, `ref`, `user_id`) VALUES
+(1, '', 'Iheanyichukwu Ikenna', 'iheanyichukwui94@gmail.com', NULL, '12345678', '$2y$10$jHAPLdAJsU7BTkRr9QBYmew8jorQ60v2zhOZVBtnFa2cJUJ9ersWe', NULL, 4, 5.00, 0.00, 0.00, 0, 100, 0, 3, '2023-09-05 20:55:23', '2023-09-07 15:42:08', NULL, NULL),
+(5, 'ref_05', 'iheanyichukwu', 'info.miztec@gmail.com', NULL, '12345678', '$2y$10$GaLwIGQzNTHaVS2JDPi/ie4Het63QReeGveHXuot2yaIp3w5hVwha', NULL, 2, 1.00, 0.00, 60.00, 0, 100, 1, 3, '2023-09-07 16:20:19', '2023-09-16 16:11:35', NULL, NULL),
+(6, 'ref_06', 'iheanyichukwu ikenna', 'ikenna@ijidev.com', NULL, '12345678', '$2y$10$dARJ0ohGXzNLRyb7YaJQrOtUKp6eVGpW640ijrx9xi26jqqb3umVC', NULL, 30, 0.00, 0.00, 0.00, 0, 100, 0, 2, '2023-09-13 06:40:48', '2023-09-13 06:40:48', NULL, 5);
 
 -- --------------------------------------------------------
 
@@ -392,10 +424,18 @@ CREATE TABLE `user_payments` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `user_id` bigint(20) UNSIGNED NOT NULL,
   `wallet` varchar(255) NOT NULL,
-  `addess` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `user_payments`
+--
+
+INSERT INTO `user_payments` (`id`, `user_id`, `wallet`, `address`, `created_at`, `updated_at`) VALUES
+(2, 5, 'TRC20', 'jkhbfcjhbfbhbfiuewhfibeifrf', '2023-09-08 13:44:33', '2023-09-09 18:04:08'),
+(4, 6, 'TRC20', 'jkhbfcjhbfbhbfiuewhfibeifrf', '2023-09-16 06:56:13', '2023-09-16 06:56:13');
 
 -- --------------------------------------------------------
 
@@ -406,12 +446,22 @@ CREATE TABLE `user_payments` (
 CREATE TABLE `withdrawals` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `user_id` bigint(20) UNSIGNED NOT NULL,
-  `wallet_id` bigint(20) UNSIGNED NOT NULL,
   `amount` double(8,2) NOT NULL,
   `status` enum('approved','declined','pending') NOT NULL DEFAULT 'pending',
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `wallet_id` bigint(20) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `withdrawals`
+--
+
+INSERT INTO `withdrawals` (`id`, `user_id`, `amount`, `status`, `created_at`, `updated_at`, `wallet_id`) VALUES
+(1, 5, 10.00, 'approved', '2023-09-09 18:50:48', '2023-09-10 13:05:40', 2),
+(2, 5, 20.00, 'approved', '2023-09-09 19:07:10', '2023-09-10 13:05:47', 2),
+(3, 6, 0.00, 'pending', '2023-09-15 16:18:24', '2023-09-15 16:18:24', 2),
+(4, 5, 20.00, 'pending', '2023-09-16 16:11:35', '2023-09-16 16:11:35', 2);
 
 --
 -- Indexes for dumped tables
@@ -529,15 +579,15 @@ ALTER TABLE `tiers`
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `users_email_unique` (`email`),
-  ADD KEY `users_user_id_foreign` (`user_id`),
-  ADD KEY `users_tier_id_foreign` (`tier_id`);
+  ADD KEY `users_tier_id_foreign` (`tier_id`),
+  ADD KEY `users_ref_foreign` (`ref`),
+  ADD KEY `users_user_id_foreign` (`user_id`);
 
 --
 -- Indexes for table `user_payments`
 --
 ALTER TABLE `user_payments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_payments_user_id_foreign` (`user_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `withdrawals`
@@ -555,7 +605,7 @@ ALTER TABLE `withdrawals`
 -- AUTO_INCREMENT for table `deposits`
 --
 ALTER TABLE `deposits`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -567,25 +617,25 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `faqs`
 --
 ALTER TABLE `faqs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `payment_infos`
 --
 ALTER TABLE `payment_infos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `permissions`
@@ -609,31 +659,31 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `settings`
 --
 ALTER TABLE `settings`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tiers`
 --
 ALTER TABLE `tiers`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `user_payments`
 --
 ALTER TABLE `user_payments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `withdrawals`
 --
 ALTER TABLE `withdrawals`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -674,21 +724,16 @@ ALTER TABLE `role_user`
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
+  ADD CONSTRAINT `users_ref_foreign` FOREIGN KEY (`ref`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `users_tier_id_foreign` FOREIGN KEY (`tier_id`) REFERENCES `tiers` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   ADD CONSTRAINT `users_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `user_payments`
---
-ALTER TABLE `user_payments`
-  ADD CONSTRAINT `user_payments_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `withdrawals`
 --
 ALTER TABLE `withdrawals`
   ADD CONSTRAINT `withdrawals_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `withdrawals_wallet_id_foreign` FOREIGN KEY (`wallet_id`) REFERENCES `user_payments` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `withdrawals_wallet_id_foreign` FOREIGN KEY (`wallet_id`) REFERENCES `user_payments` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
